28a878a9c8af9f3b260d3fec890846cbfb35832d
Rabin Chakraborty <110186169+RabinChakraborty@users.noreply.github.com>
2023-04-15 15:33:54 +0530

    Add files via upload

---
+++ TypeScript/dist/First.js
@@ -0,0 +1,7 @@
+"use strict";
+let num = 20;
+console.log(num);
+if (num < 50) {
+    console.log('hello');
+}
+//# sourceMappingURL=First.js.map
---
+++ TypeScript/src/First.ts
@@ -0,0 +1,9 @@
+let num: number = 20;
+console.log(num);
+//First typeScript program learned to assign a number variable.
+
+//2.debug typescript in vsCode.
+// add this line below program while debug in launch.json="preLaunchTask":"tsc:build - tsconfig.json"
+if (num < 50) {
+  console.log('hello');
+}
